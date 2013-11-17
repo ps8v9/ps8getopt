57aba0778344a66b64631e6ad80b7662f15a370a
Matthew Fisher <ps8v9@users.noreply.github.com>
2013-11-17 11:30:14 -0500

    Update ps8posix.h
    ... after renaming the repo.

--- ps8posix.h
+++ ps8posix.h
@@ -1,16 +1,16 @@
 /*
- *  ps8posix.c: A few POSIX functions that MSVC doesn't provide.
+ *  ps8getopt.h: An implementation of the getopt() function.
  *
  *  For specifications, see POSIX1.2008 standard, vol. 3, section 3.
  *  URL: http://pubs.opengroup.org/onlinepubs/9699919799/
  *
  *  AUTHOR  : Matthew J. Fisher
- *  REPO    : https://github.com/ps8v9/ps8posix
+ *  REPO    : https://github.com/ps8v9/ps8getopt
  *  LICENSE : This is free and unencumbered software released into the public
  *            domain. See the LICENSE file for further details.
  */
-#ifndef PS8POSIX_H
-#define PS8POSIX_H
+#ifndef PS8_GETOPT_H
+#define PS8_GETOPT_H
 
 int getopt(int argc, char* const argv[], const char *optstring);
 
@@ -19,4 +19,4 @@ int opterr;
 int optind;
 int optopt;
 
-#endif /* PS8POSIX_H */
+#endif /* PS8_GETOPT_H */
