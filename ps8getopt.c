7f0b4103286d7de8979dc39bd8d0cf208dc431f3
Matthew Fisher <mjf2072126628@gmail.com>
2013-11-17 15:13:08 -0500

    Set up some further tests in test.c.

--- ps8getopt.c
+++ ps8getopt.c
@@ -9,7 +9,8 @@
  *  LICENSE : This is free and unencumbered software released into the public
  *            domain. See the LICENSE file for further details.
  */
-#include <ctype.h>    /* for isalnum() */
+#include <ctype.h> /* for isalnum() */
+#include <stdio.h>
 
 #include "ps8getopt.h"
 
@@ -18,16 +19,29 @@ int ps8_opterr;
 int ps8_optind = 1;
 int ps8_optopt;
 
+/*
+ *  TODO:
+ *   - Start with the simplest case.
+ *   - Ignore chars in optstring that are not a colon or alnum char.
+ *   - Ignore all colons in optstring.
+ *   - Ignore opterr and optopt.
+ *   - Assume that there are no grouped option chars in argv.
+ *   - Assume that there are no option arguments in argv.
+ *   - Return the next option char from argv that matches a char from optstring.
+ */
+
 int ps8_getopt(int argc, char* const argv[], const char *optstring)
 {
-    return '\*'; /* decimal 42, which is an invalid result */
+    return '*'; /* decimal 42, which is an invalid result */
 }
 
 /*
- *  Some notes on the specs for getopt():
+ *  Some notes on the specs for getopt(), and the implementation of
+ *  ps8_getopt():
  *
  *  - single-char options only
- *  - allowed chars are A-Z, a-z, 0-9 (can use isalnum() for this)
+ *  - allowed chars are A-Z, a-z, 0-9 (could use isalnum() for this)
+ *  - the implementation may accept other characters as an extension
  *  - opt delimiter is a hyphen (-)
  *  - multiple opts may be grouped behind a single delimiter
  *  - an opt that takes an optarg terminates the group
@@ -40,7 +54,7 @@ int ps8_getopt(int argc, char* const argv[], const char *optstring)
  *  - optstring is a string of recognized opt characters
  *  - a colon after a character indicates that it takes an option
  *  - optind is the index of the next element of argv to be processed
- *  - optind shall be initialized to 1 "by the system"
+ *  - DONE: optind shall be initialized to 1 "by the system"
  *  - getopt shall update optind when done with each element of argv
  *  - not specified: what happens if app sets optind to 0 before calling getopt
  *  - not specified: when an element contains multiple opt chars, how does
--- test.c
+++ test.c
@@ -1,32 +1,102 @@
 /*
- *  test.c: A main program to test ps8getopt.c.
+ *  test.c: A main program to test ps8getopt.c against the real getopt.
  *
  *  AUTHOR  : Matthew J. Fisher
  *  REPO    : https://github.com/ps8v9/ps8getopt
  *  LICENSE : This is free and unencumbered software released into the public
  *            domain. See the LICENSE file for further details.
  */
-#include <assert.h>
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
+
 #include <unistd.h> /* exists on POSIX systems and comes with MINGW */
-
-#include "ps8getopt.h"
-
 extern char *optarg;
 extern int opterr;
 extern int optind;
 extern int optopt;
 
+#include "ps8getopt.h"
 extern char *ps8_optarg;
 extern int ps8_opterr;
 extern int ps8_optind;
 extern int ps8_optopt;
 
-int main(int argc, char *argv[])
-{
-    printf("optind: %d; ps8_optind: %d\n", optind, ps8_optind);
-    assert(optind == ps8_optind);
+void info1(const char *optstring);
+void info2(int argc, char* const argv[]);
 
-    char *optstring = "abc";
-    assert(getopt(argc, argv, optstring) == ps8_getopt(argc, argv, optstring));
+bool test1();
+bool test2(int argc, char* const argv[], const char *optstring);
+
+int main()
+{
+    bool passed; /* Were all tests passed? */
+    char *optstring = ":a-*$b:c";
+
+    info1(optstring);
+    passed = test1();
+
+    /* Test with no command line options or operands. */
+    int argc1 = 1;
+    char *argv1[] = { "test" };
+    info2(argc1, argv1);
+    passed &= test2(argc1, argv1, optstring);
+
+    /* Test with no command line options, and 1 operand. */
+    int argc2 = 2;
+    char *argv2[] = { "test", "abc"};
+    info2(argc2, argv2);
+    passed &= test2(argc2, argv2, optstring);
+
+    /* Test with 1 command line option, and 1 operand. */
+    int argc3 = 3;
+    char *argv3[] = { "test", "-a", "bc"};
+    info2(argc3, argv3);
+    passed &= test2(argc3, argv3, optstring);
+
+}
+
+/* info1: Print the option chars from optstring that must be supported. */
+void info1(const char *optstring)
+{
+    printf("optstring: %s\n", optstring);
+    printf("supported chars in optstring: ");
+    for (int i = 0; optstring[i] != '\0'; ++i)
+       if (isalnum(optstring[i]) || optstring[i] == ':')
+           putchar(optstring[i]);
+    printf("\n\n");
+}
+
+/* info2: Echo the elements of the argv vector. */
+void info2(int argc, char* const argv[])
+{
+    for (int i = 0; i < argc; ++ i)
+        printf("argv[%d]: %s\n", i, argv[i]);
+    putchar('\n');
+}
+
+/* test1: Was ps8_optind correctly initialized? */
+bool test1()
+{
+    if(ps8_optind == optind)
+        return true;
+
+    fprintf(stderr, "Failed the initialization test:\n");
+    fprintf(stderr, "  optind: %d; ps8_optind: %d\n", optind, ps8_optind);
+    return false;
+}
+
+/* test2: Do results from ps8_getopt & getopt match with no opts? */
+bool test2(int argc, char* const argv[], const char *optstring)
+{
+    int a = getopt(argc, argv, optstring);
+    int b = ps8_getopt(argc, argv, optstring);
+
+    if(ps8_optind == optind)
+        return true;
+
+    fprintf(stderr, "Failed the validation test:\n");
+    fprintf(stderr, "  getopt returned: %d\n", a);
+    fprintf(stderr, "  getopt returned: %d\n", b);
+    return false;
 }
