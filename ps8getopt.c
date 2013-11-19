04924afbac929a27ff91775bf4ca4c6e97c49aef
Matthew Fisher <mjf2072126628@gmail.com>
2013-11-18 21:41:10 -0500

    Refactored for clarity.

--- ps8getopt.c
+++ ps8getopt.c
@@ -20,20 +20,18 @@
 char *ps8_optarg;
 int ps8_opterr = 1;
 int ps8_optind = 1;
-int ps8_optopt;
+int ps8_optopt = 63;
 
 static int optchar_offset = 1;
 
 int ps8_getopt(int argc, char* const argv[], const char *optstring)
 {
-    char *optgroup;
-    char  optchar;
-    int   optstr_len;
+    /* Sanity checks. */
 
     if (argc <= ps8_optind)
         return -1;
 
-    optgroup = argv[ps8_optind];
+    char *optgroup = argv[ps8_optind];
 
     if (optgroup == NULL || optgroup[0] != '-' || (strcmp(optgroup, "-") == 0))
         return -1;
@@ -42,101 +40,54 @@ int ps8_getopt(int argc, char* const argv[], const char *optstring)
         return -1;
     }
 
-    optchar = optgroup[optchar_offset];
-    optstr_len = strlen(optstring);
+    /* Try to match next option char from argv to a char in optstring. */
 
-/*  TODO:
- *  If the option takes an argument, getopt() shall set the variable optarg to
- *  point to the option-argument as follows:
- *    1. If the option was the last character in the string pointed to by an
- *       element of argv, then optarg shall contain the next element of argv,
- *       and optind shall be incremented by 2. If the resulting value of optind
- *       is greater than argc, this indicates a missing option-argument, and
- *       getopt() shall return an error indication.
- *    2. Otherwise, optarg shall point to the string following the option
- *       character in that element of argv, and optind shall be incremented
- *       by 1.
-*/
-    for (int i = 0; i < optstr_len; ++i)
+    char optchar = optgroup[optchar_offset];
+    bool matched = false;
+    int optstr_len = strlen(optstring);
+    int i;
+
+    for (i = 0; i < optstr_len; ++i) {
         if (optstring[i] == optchar) {
-            if (strlen(optgroup) == optchar_offset + 1) {
-                printf("???\n");
-                ps8_optind++;
-                optchar_offset = 1;
-            } else {
-                if (optstring[i + 1] == ':') {
-                    /* Option takes an argument. */
-                    printf("Option takes an argument.\n");
-                    if (optchar_offset + 1 == strlen(optgroup)) {
-                        /* Option was the last char in optgroup. */
-                        ps8_optarg = argv[ps8_optind + 1];
-                        ps8_optind += 2;
-                        if (ps8_optind > argc) {
-                            /* How to "return an error indication" here? */
-                        }
-                    } else {
-                        ps8_optarg = optgroup + optchar_offset + 1;
-                        ps8_optind++;
-                        optchar_offset = 1;
-                    }
-                } else {
-                    optchar_offset++;
-                }
-            }
-            return optchar;
+            matched = true;
+            break;
         }
+    }
 
-    return '?';
+    if (! matched)
+        return '?';
+
+    /* Found a valid opt char. What do we know about it? */
+
+    bool takes_arg = (optstring[i + 1] == ':');
+    bool terminates_optgroup = (optgroup[optchar_offset + 1] == '\0');
+
+    /* Do the specified things! */
+
+    if (!takes_arg) {
+        if (terminates_optgroup) {
+            ps8_optind++;
+            optchar_offset = 1;
+        } else {
+            optchar_offset++;
+        }
+        return optchar;
+    }
+
+    if (terminates_optgroup) {
+        ps8_optarg = argv[ps8_optind + 1];
+        ps8_optind += 2;
+        if (ps8_optind > argc) {
+            /* Option argument is missing. */
+            /* What does the real getopt do here? */
+            return '?';
+        }
+    } else {
+        ps8_optarg = optgroup + optchar_offset + 1;
+        ps8_optind++;
+        optchar_offset = 1;
+    }
+
+    return optchar;
+
 }
-
-/*
- *  Some notes on the specs for getopt(), and the implementation of
- *  ps8_getopt():
- *
- *  - getopt() shall return -1 when all command line options are parsed
- *  - single-char options only
- *  - allowed chars are A-Z, a-z, 0-9 (could use isalnum() for this)
- *  - the implementation may accept other characters as an extension
- *  - opt delimiter is a hyphen (-)
- *  - multiple opts may be grouped behind a single delimiter
- *  - an opt that takes an optarg terminates the group
- *  - an optional optarg is part of the same argument as the option
- *  - a mandatory optarg is the next argument
- *  - opts precede operands
- *  - the -- delimiter indicates no more opts
- *  - argc is the argument count as received by main
- *  - argv is the argument array as received by main
- *  - optstring is a string of recognized opt characters
- *  - a colon after a character indicates that it takes an option
- *  - optind is the index of the next element of argv to be processed
- *  - optind shall be initialized to 1 "by the system"
- *  - getopt shall update optind when done with each element of argv
- *  - not specified: what happens if app sets optind to 0 before calling getopt
- *  - not specified: when an element contains multiple opt chars, how does
- *                   getopt determine which options have already been processed
- *  - getopt shall return the next opt char (if found) that matches a char in
- *    optstring (if there is a match)
- *  - if opt that takes arg was last char in an element of argv, optarg shall
- *    contain the next element of argv, and optind += 2 (and if this exceeds
- *    argc, getopt shall return an error)
- *  - otherwise, optarg shall point to the string following the opt char in that
- *    element of argv, and optind += 1
- *  - if when getopt is called:
- *       argv[optind] != NULL
- *      *argv[optind] != '-'
- *       strncmp(argv[optind], "-", 1) == 0
- *    then getopt shall return -1 without incrementing optind
- *  - if strcmp(argv[optind], "--", getopt shall return -1 after incrementing
- *    optind
- *  - if an opt char that is not in optstring is found, return '?'
- *  - if (optarg is missing) {
- *      if optstring[0] was ':'
- *        return ':'
- *      else
- *        return '?'
- *      optopt = opt char for which optarg was missing
- *      if app did not set opterr to 0 && optstring[0] != ':'
- *        print a diagnostic message to stderr in the format specified for the getopts
- *          utility
- *  - getopt need not be thread safe
- */
